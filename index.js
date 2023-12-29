84b15399a175946787b368ffa7139e3d29d31210
bidemidone1 <bidemidone@gmail.com>
2023-12-29 20:41:37 +0100

    modified the index.js codes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const outcome = document.querySelector("#result");
-let num1 = document.getElementById("num1").value;
+const num1 = document.getElementById("num1");
 const num2 = document.querySelector("#num2");
 
 // let a = "5";
 // let b = "2";
 
-let a = num1;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
 // console.log(a + b);
 // console.log(a - b);
@@ -15,37 +15,31 @@ let b = 2;
 // console.log(a % b);
 
 function add() {
-    let result = a + b;
+    let result = parseInt(num1.value) + parseInt(num2.value);
     outcome.innerHTML = result
-    console.log(num1);
-    // return result;
-    // console.log(result);
+    console.log(result);
 }
 
 function subtract() {
-    let result = a - b;
+    let result = parseInt(num1.value) - parseInt(num2.value);
+    outcome.innerHTML = result
     console.log(result);
-    // return result;
-    // console.log(result);
 }
 
 function multiply() {
-    let result = a * b;
+    let result = parseInt(num1.value) * parseInt(num2.value);
+    outcome.innerHTML = result
     console.log(result);
-    // return result;
-    // console.log(result);
 }
 
 function divide() {
-    let result = a / b;
+    let result = parseInt(num1.value) / parseInt(num2.value);
+    outcome.innerHTML = result
     console.log(result);
-    // return result;
-    // console.log(result);
 }
 
 function modulus() {
-    let result = a % b;
+    let result = parseInt(num1.value) % parseInt(num2.value);
+    outcome.innerHTML = result
     console.log(result);
-    // return result;
-    // console.log(result);
 }
